9be4386346eca645ec550afec3c9ad19a65deddf
Jorge Padron <Jorge.padron.cordova@hotmail.com>
2021-03-10 17:14:53 -0600

    cambios pequeños

--- BCK_Axosnet/Controllers/DivisaController.cs
+++ BCK_Axosnet/Controllers/DivisaController.cs
@@ -6,83 +6,99 @@ using System.Linq;
 using System.Threading.Tasks;
 using BCK_Axosnet.Model;
 using BCK_Axosnet.Data;
+using Microsoft.Extensions.Logging;
+using BCK_Axosnet.Data;
 
 namespace BCK_Axosnet.Controllers
 {
-    public class DivisaController   : Controller  
+    [ApiController]
+    [Route("[controller]")]
+    public class DivisaController   : ControllerBase
     {
+       
         // GET: DivisaController
         private readonly DivisaData _divisaData;
-
+        
 
         [HttpGet]
         // GET: DivisaController/Details/5
-        public Task<String> Details(int id)
+        public DivisaModel Details(int id)
         {
-            return _divisaData.GetAll();
+            DivisaData _divisaData = new DivisaData();
+
+            DivisaModel _divisaModel = new DivisaModel();
+
+            _divisaData.GetAll();
+
+            _divisaModel.IdDivisa = 1;
+            _divisaModel.Divisa = "U.S.D";
+            _divisaModel.NombreCorto = "Nombe corto papá";
+
+
+            return _divisaModel;
         }
 
-        // GET: DivisaController/Create
-        public async Task<ActionResult> Create()
-        {
-            return  View();
-        }
+    //    // GET: DivisaController/Create
+    //    public async Task<ActionResult> Create()
+    //    {
+    //        return  View();
+    //    }
 
-        // POST: DivisaController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    //    // POST: DivisaController/Create
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<ActionResult> Create(IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
 
-        // GET: DivisaController/Edit/5
-        public async Task<ActionResult> Edit(int id)
-        {
-            return  View();
-        }
+    //    // GET: DivisaController/Edit/5
+    //    public async Task<ActionResult> Edit(int id)
+    //    {
+    //        return  View();
+    //    }
 
-        // POST: DivisaController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    //    // POST: DivisaController/Edit/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Edit(int id, IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
 
-        // GET: DivisaController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+    //    // GET: DivisaController/Delete/5
+    //    public ActionResult Delete(int id)
+    //    {
+    //        return View();
+    //    }
 
-        // POST: DivisaController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+    //    // POST: DivisaController/Delete/5
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public ActionResult Delete(int id, IFormCollection collection)
+    //    {
+    //        try
+    //        {
+    //            return RedirectToAction(nameof(Index));
+    //        }
+    //        catch
+    //        {
+    //            return View();
+    //        }
+    //    }
     }
 }
